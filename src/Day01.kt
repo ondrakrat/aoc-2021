d0a5a5a44897458b60bc5f0e4260ffb8d196fd43
Ondrej Kratochvil <onkratochvil@purestorage.com>
2021-12-03 10:23:03 +0100

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,14 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("input/day1/test")
-    check(part1(testInput) == 10)
-
     val input = readInput("input/day1/data")
-    println(part1(testInput))
-    println(part2(input))
+
+    var previousDepth = Int.MAX_VALUE
+    var increased = 0
+    for (depth in input.map { it.trim().toInt() }) {
+        if (depth > previousDepth) {
+            ++increased
+        }
+        previousDepth = depth
+    }
+    println("The depth has increased $increased times")
 }
