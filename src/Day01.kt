c79e4411b800c6be5b45649cf33ad53d65607258
Ondrej Kratochvil <onkratochvil@purestorage.com>
2021-12-03 11:06:34 +0100

    Day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,6 +2,11 @@ fun main() {
     val testInput = readInput("input/day1/test")
     val input = readInput("input/day1/data")
 
+    //part1(input)
+    part2(input)
+}
+
+private fun part1(input: List<String>) {
     var previousDepth = Int.MAX_VALUE
     var increased = 0
     for (depth in input.map { it.trim().toInt() }) {
@@ -11,4 +16,13 @@ fun main() {
         previousDepth = depth
     }
     println("The depth has increased $increased times")
+}
+
+private fun part2(input: List<String>) {
+    val transformedInput = input
+        .map { it.trim().toInt() }
+        .windowed(3) // by default doesn't keep partial windows
+        .map { it.sum() }
+        .map { it.toString() }
+    part1(transformedInput)
 }
