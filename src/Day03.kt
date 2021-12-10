9d2eb7a9a75b1cc3b547decf9025d0fe41bc908b
Ondrej Kratochvil <onkratochvil@purestorage.com>
2021-12-10 10:18:37 +0100

    Day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -4,8 +4,8 @@ fun main() {
     val testInput = readInput("input/day3/test")
     val input = readInput("input/day3/data")
 
-    part1(input)
-//    part2(input)
+//    part1(input)
+    part2(input)
 }
 
 private fun part1(input: List<String>) {
@@ -32,4 +32,28 @@ private fun part1(input: List<String>) {
 }
 
 private fun part2(input: List<String>) {
+    val oxygen = getRating(input, true)
+    val co2 = getRating(input, false)
+    println(Integer.parseInt(oxygen, 2) * Integer.parseInt(co2, 2))
 }
+
+private fun getRating(input: List<String>, most: Boolean): String {
+    var zeroFrequency = 0
+    var oneFrequency = 0
+    var candidates = listOf(*input.toTypedArray())
+    var currentPos = 0
+    while (candidates.size > 1) {
+        oneFrequency = onesOnPosition(candidates, currentPos)
+        zeroFrequency = candidates.size - oneFrequency
+        val filteredDigit = if (most) {
+            if (oneFrequency >= zeroFrequency) '1' else '0'
+        } else {
+            if (zeroFrequency <= oneFrequency) '0' else '1'
+        }
+        candidates = candidates.filter { it.toCharArray()[currentPos] == filteredDigit }
+        ++currentPos
+    }
+    return candidates[0]
+}
+
+private fun onesOnPosition(input: List<String>, position: Int) = input.sumOf { it.toCharArray()[position].digitToInt() }
